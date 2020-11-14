cc2c7ad5c8b398eaff0beea082f86f3d8af3844c
SBen-IV <sinneo@fi.uba.ar>
2020-11-14 18:46:04 -0300

    Correccion issue #6

--- AnalizadorInstruccion.cpp
+++ AnalizadorInstruccion.cpp
@@ -1,59 +1,47 @@
 #include "AnalizadorInstruccion.h"
 #include "Instruccion.h"
+
 #include <list>
+#include <sstream>
 
 #define RET_OPCODE "ret"
 #define VACIO ""
+#define MAX_ARGS 3
 
-std::size_t obtenerEtiqueta(const std::string& linea, std::string& etiqueta) {
-	std::size_t pos_fin = linea.find(':');
-
-	if (pos_fin == std::string::npos) {
-		etiqueta = VACIO;
-		pos_fin = linea.find_first_not_of(' ');
-	} else {
-		etiqueta = linea.substr(0, pos_fin);
-		pos_fin = linea.find_first_not_of(' ', pos_fin + 1);
+static void obtenerEtiqueta(std::istringstream& linea,
+							const std::string& _linea,
+							std::string& etiqueta) {
+	if (_linea.find(':') != std::string::npos) {
+		std::getline(linea, etiqueta, ':');
 	}
-
-	return pos_fin;
 }
 
-std::size_t obtenerOpcode(const std::string& linea, std::string& opcode) {
-	std::size_t pos_fin = linea.find(' ');
 
-	if (pos_fin == std::string::npos) {
-		opcode = linea;
-		pos_fin = linea.length() - 1;
-	} else {
-		opcode = linea.substr(0, pos_fin);
-		pos_fin = linea.find_first_not_of(' ', pos_fin);
+static void obtenerArgumentos(std::istringstream& linea, 
+								std::list<std::string>& argumentos) {
+	std::string aux;
+	for (std::size_t i = 0; i < MAX_ARGS && !linea.eof(); ++i) {
+		linea >> aux;
+		if (aux != VACIO) {
+			if (aux.back() == ',') {
+				aux.pop_back();
+			}
+
+			argumentos.push_back(aux);
+		}
 	}
-
-	return pos_fin;
 }
 
-void obtenerSaltos(const std::string& linea,
-					std::list<std::string>& argumentos) {
-	std::size_t pos_inicio = 0, pos_fin = linea.find(',');
-
-	do {
-		argumentos.push_back(linea.substr(pos_inicio, pos_fin-pos_inicio));
-		pos_inicio = linea.find_first_not_of(", ", pos_fin);
-		pos_fin = linea.find(',', pos_inicio);
-	} while (pos_inicio != std::string::npos);
-}
-
-Instruccion AnalizadorInstruccion::identificar(const std::string& linea) {
+Instruccion AnalizadorInstruccion::identificar(const std::string& _linea) {
+	std::istringstream linea(_linea);
 	std::string etiqueta, opcode;
 	std::list<std::string> argumentos;
-	std::size_t pos_inicio = 0;
 
-	pos_inicio += obtenerEtiqueta(linea, etiqueta);
-	pos_inicio += obtenerOpcode(&linea[pos_inicio], opcode);
-	obtenerSaltos(&linea[pos_inicio], argumentos);
+	obtenerEtiqueta(linea, _linea, etiqueta);
+	linea >> opcode;
+	obtenerArgumentos(linea, argumentos);
 
-	Instruccion instruccion(linea, etiqueta, opcode, argumentos);
+	Instruccion instruccion(_linea, etiqueta, opcode, argumentos);
 
 	return instruccion;
 }
@@ -61,3 +49,9 @@ Instruccion AnalizadorInstruccion::identificar(const std::string& linea) {
 AnalizadorInstruccion::AnalizadorInstruccion() {}
 
 AnalizadorInstruccion::~AnalizadorInstruccion() {}
+/*
+	std::size_t pos_inicio = 0;
+
+	pos_inicio += obtenerEtiqueta(linea, etiqueta);
+	pos_inicio += obtenerOpcode(&linea[pos_inicio], opcode);
+	obtenerSaltos(&linea[pos_inicio], argumentos);*/
